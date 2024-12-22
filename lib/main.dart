4bcbafbc74556641a0c2c1f6827b978c5f910be7
si3aa <mahmoud22said22@gmail.com>
2024-12-22 15:37:21 +0200

    delete fontPackage

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:bookly/Features/splash/presentation/views/splash_view.dart';
 import 'package:bookly/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(const BooklyApp());
@@ -15,8 +15,7 @@ class BooklyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor,
-      textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor,),
       
       home: const SplashView(),
     );
